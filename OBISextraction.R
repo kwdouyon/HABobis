f56cb025dfc55e4a598b7ac2b7257d533ffbb902
Kenneth W. Douyon <90868698+kwdouyon@users.noreply.github.com>
2021-09-21 14:14:59 -0400

    Added Loop for OBIS Extraction

--- OBISextraction.R
+++ OBISextraction.R
@@ -1,12 +1,28 @@
 #' function retrieves OBIS data by species
-#' 
+
 #' @param species chr, name of species to retrieve
 #' @param path chr, path to where data is saved
 #' @return tibble of species data
+
+library(dplyr)
+
+hab <- read.csv('~/Documents/data/habobis/habslist.csv')
+
+hab.species.only <- hab %>% filter(grepl(' ', ScientificName))
+
+species.list <- hab.species.only$ScientificName
+
+for (j in 1:319) {
+  taxon.data <- robis::occurrence(as.character(species.list[j]))
+fetch_species(as.character(species.list[j]))
+}
+
+
 fetch_species <- function(species = 'Dinophysis acuta',
-                          path = "~/Documents/data/habobis"){
+                          path = "~/Documents/data/habobis"/"rawdata"){
   taxon.data <- robis::occurrence(species[1])
   taxon.name = gsub(" ","_", tolower(species))
   readr::write_csv(taxon.data, file.path(path[1],paste0(taxon.name,".csv.gz")))
   return(taxon.data)
 } 
+
---
+++ plots.R
@@ -0,0 +1,24 @@
+#' function retrieves OBIS data by species
+
+#' @param species chr, name of species to retrieve
+#' @param path chr, path to where data is saved
+#' @return tibble of species data
+
+
+sst_plot <- function(species = 'Dinophysis acuta',
+                     path = "~/Documents/data/habobis"/"sstplot"){
+  taxon.data <- robis::occurrence(species[1])
+  taxon.name = gsub(" ","_", tolower(species))
+  readr::write_csv(taxon.data, file.path(path[1],paste0(taxon.name,".csv.gz")))
+  return(taxon.data)
+} 
+
+
+sss_plot <- function(species = 'Dinophysis acuta',
+                     path = "~/Documents/data/habobis"/"sssplot"){
+  taxon.data <- robis::occurrence(species[1])
+  taxon.name = gsub(" ","_", tolower(species))
+  readr::write_csv(taxon.data, file.path(path[1],paste0(taxon.name,".csv.gz")))
+  return(taxon.data)
+} 
+
